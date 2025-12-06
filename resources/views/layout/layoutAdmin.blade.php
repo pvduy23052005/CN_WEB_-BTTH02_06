bcd9190684e39acab6af7f472cdc3a86ec7bbc0d
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-12-06 17:06:18 +0700

    Add css

--- resources/views/layout/layoutAdmin.blade.php
+++ resources/views/layout/layoutAdmin.blade.php
@@ -4,10 +4,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="{{ asset('css/base.css')}}">
+  <link rel="stylesheet" href="{{asset("css/style.css")}}">
+
   <title>{{$title}}</title>
 </head>
 <body>
   @include('layout.sidebar')
   @yield('main-content')
+  <script src="{{ asset('js/script.js') }}"></script>
 </body>
 </html>
