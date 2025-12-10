ff6d8bda20c26b6ba7d1d0c07a79152619d24412
pvduy23052005 <162944866+pvduy23052005@users.noreply.github.com>
2025-12-10 16:28:31 +0700

    Danh sách người dùng

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -2,14 +2,6 @@
 
 @section('main-content')
   <div class="main-content">
-    @foreach ($courses as $course)
-      <div style="border: 1px solid #ccc; padding: 10px; margin-bottom: 10px;">
-          <h3>{{ $course->title }}</h3>
-          <p>{{ $course->description }}</p>
-          <p>Giá: {{ $course->price }}</p>
-          <p>Thời lượng: {{ $course->duration_weeks }} tuần</p>
-          <p>Cấp độ: {{ $course->level }}</p>
-      </div>
-    @endforeach
+    <h1>Dashboard</h1>
   </div>
 @endsection
